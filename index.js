0167a26a35d6a98d31694f3ef5be747f6f127562
David111922 <salve62@yahoo.com>
2024-05-02 00:18:15 -0700

    Adding remaining stub routes and README file

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('/places, require'('./controller/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World!')
 })
-app.get('*,', (require, res) => {
-    res.status(404) .send('<h1>404 Page</h1>')
+
+app.get('*', (req, res) => {
+    res.status(404) .send('404 page')
 
 })
 
