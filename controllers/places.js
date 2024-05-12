9144754744553b435b07a797a9d1184679773b9d
David111922 <salve62@yahoo.com>
2024-05-12 16:07:52 -0700

    Form Data is going through!

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,34 @@
 const router = require('express').Router();
+const places = require('../models/places.js')
 router.get('/new', (req, res) => {
   res.render('places/new')
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'images/Pasta-Alfredo.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'images/pizza.jpg'
-  }]
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
+
+
+ 
   
 
   res.render('places/index', {places})
 })
 
 
- 
-    
+//  !moved to models/places.js
 
-module.exports = router
+// module.exports = router = [{
+  //     name: 'H-Thai-ML',
+  //     city: 'Seattle',
+  //     state: 'WA',
+  //     cuisines: 'Thai, Pan-Asian',
+  //     pic: 'images/Pasta-Alfredo.jpg'
+  //   }, {
+  //     name: 'Coding Cat Cafe',
+  //     city: 'Phoenix',
+  //     state: 'AZ',
+  //     cuisines: 'Coffee, Bakery',
+  //     pic: 'images/pizza.jpg'
+  //   }]   
+
