11b6d0ffb0bb195bce1403b3ad351fd06abfd794
David111922 <salve62@yahoo.com>
2024-05-13 23:25:29 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -6,7 +6,7 @@ const places = require('../models/places.js')
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
-    // Default image if one is not provided
+    
     req.body.pic = 'http://placekitten.com/400/400'
   }
   if (!req.body.city) {
@@ -20,9 +20,16 @@ router.post('/', (req, res) => {
 
 })
 
-router.get('/:id', (req, res) => {
-  res.render('places/show')
+router.get('/', (req, res) => {
+
+res.render('places/index', {places})
 })
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+
+})
+
 
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -34,32 +41,29 @@ res.render('error404')
 
   }
   else {
-    res.render('places/show', {place: places[id] })
+    res.render('places/show', {place: places[id], id })
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
 
 
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
+ 
 
-  // res.render('places/index', {places})
-})
 module.exports = router
 
-//  !moved to models/places.js
 
-// module.exports = router = [{
-  //     name: 'H-Thai-ML',
-  //     city: 'Seattle',
-  //     state: 'WA',
-  //     cuisines: 'Thai, Pan-Asian',
-  //     pic: 'images/Pasta-Alfredo.jpg'
-  //   }, {
-  //     name: 'Coding Cat Cafe',
-  //     city: 'Phoenix',
-  //     state: 'AZ',
-  //     cuisines: 'Coffee, Bakery',
-  //     pic: 'images/pizza.jpg'
-  //   }]   
 
