867d2b71944e482a740662b8af03b7d32b79d8d7
David111922 <salve62@yahoo.com>
2024-05-22 04:31:36 -0700

    fixed img src{data.place.pic} in show.jsx..removed quotations

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,7 @@ function show(data) {
       <main>
         <div className="row">
         <div className="col-sm-6">
-        <img src="{data.place.pic" alt="data.place.name" />
+        <img src={data.place.pic} alt={data.place.name} />
         <h3>
         Located in {data.place.city}, {data.place.state}
         </h3>
