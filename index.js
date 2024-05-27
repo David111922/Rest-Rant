c75b7a79e21ae22b8d717b3166a185bea89427e3
David111922 <salve62@yahoo.com>
2024-05-26 18:01:35 -0700

    added comment section Rant/Rave

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 /// Modules and Globals
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-module.exports.Place
-//  = require('./places')
+// module.exports.Place = require('./places');
+// 
 
-const express = require('express')
-const methodOverride = require('method-override')
+const express = require("express");
+const methodOverride = require("method-override");
 
-const app = express()
+const app = express();
 
 // Express Settings
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 // Controllers & Routes
-app.use('/places', require('./controllers/places'))
+app.use("/places", require("./controllers/places"));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('*', (req, res) => {
-    res.status(404).render('error404')
-})
+app.get("*", (req, res) => {
+  res.status(404).render("error404");
+});
 
 // Listen for Connections
-app.listen(process.env.PORT)
-
-
-
+app.listen(process.env.PORT);
 
 // require('dotenv').config()
 // const express = require('express')
@@ -53,7 +50,7 @@ app.listen(process.env.PORT)
 // })
 
 // app.get('*', (req, res) => {
-//   res.status(404).render('error404') 
+//   res.status(404).render('error404')
 // })
 
 // app.listen(process.env.PORT)
