7c66f914627324f3992382c23b4d862b5508d4c0
David111922 <salve62@yahoo.com>
2024-05-26 23:12:14 -0700

    added edit function

--- controllers/places.js
+++ controllers/places.js
@@ -77,22 +77,47 @@ router.post('/:id/comment', (req, res) => {
 
 
 
-// })
+// route that edits a place by id
 
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
+
+// Delete route
 
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
+
+// Edit route
+
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
 router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+  res.send('GET edit form stub stub')
 })
 
 router.delete('/:id/rant/:rantId', (req, res) => {
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form({ place, id }) {
     <Def>
       <main>
         <h1>Edit Place</h1>
-        <form method="POST" action={`/places/${id}?_method=PUT`}>
+        <form method="POST" action={`/places/${place.id}?_method=PUT`}>
           <div className="row">
             <div className="form-group col-sm-6">
               <label htmlFor="name">Place Name</label>
@@ -18,55 +18,69 @@ function edit_form({ place, id }) {
                 defaultValue={place.name}
               />
             </div>
-            <div className="form-group col-sm-6">
-              <label htmlFor="pic">Place City</label>
-              <input
-                id="city"
-                type="text"
-                name="city"
-                required
-                defaultValue={place.city}
+
+            <div className="form-group col-sm-4">
+              <label htmlFor="founded">Founded</label>
+              <input className="form-control"
+                id="founded"
+                name="founded"
+                value={place.founded}
               />
+
             </div>
           </div>
-          <div className="row">
-            <div className="form-group col-sm-6">
-              <label htmlFor="pic">Place State</label>
-              <input
-                id="state"
-                type="text"
-                name="state"
-                required
-                defaultValue={place.state}
-              />
-            </div>
-            <div className="form-group col-sm-6">
-              <label htmlFor="pic">Place Picture</label>
-              <input
-                id="pic"
-                type="text"
-                name="pic"
-                required
-                defaultValue={place.pic}
-              />
-            </div>
+
+
+
+          <div className="form-group col-sm-6">
+            <label htmlFor="pic">Place City</label>
+            <input
+              id="city"
+              type="text"
+              name="city"
+              required
+              defaultValue={place.city}
+            />
           </div>
-          <div className="row">
-            <div className="form-group col-sm-6">
-              <label htmlFor="pic">Place Cuisines</label>
-              <input
-                id="cuisines"
-                type="text"
-                name="cuisines"
-                required
-                defaultValue={place.cuisines}
-              />
-            </div>
+        
+        <div className="row">
+          <div className="form-group col-sm-6">
+            <label htmlFor="pic">Place State</label>
+            <input
+              id="state"
+              type="text"
+              name="state"
+              required
+              defaultValue={place.state}
+            />
           </div>
-          <input type="submit" />
-        </form>
-      </main>
-    </Def>
+          <div className="form-group col-sm-6">
+            <label htmlFor="pic">Place Picture</label>
+            <input
+              id="pic"
+              type="text"
+              name="pic"
+              required
+              defaultValue={place.pic}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="form-group col-sm-6">
+            <label htmlFor="pic">Place Cuisines</label>
+            <input
+              id="cuisines"
+              type="text"
+              name="cuisines"
+              required
+              defaultValue={place.cuisines}
+            />
+          </div>
+        </div>
+        <input classNamet="btn btn-primary" type="submit" value="Update Place" />
+      </form>
+    </main>
+    </Def >
   );
 }
 
