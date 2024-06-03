2b347da76a7d99e2f3701bc53d1b5b4ae4dcedb1
David111922 <salve62@yahoo.com>
2024-06-03 00:49:12 -0700

    latest change to connectivity

--- models/index.js
+++ models/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 // !elliminated use Unified topology.???
-mongoose.connect(process.env.MONGO_URI
- 
-)
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB:', process.env.MONGO_URI);
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  }); 
+
 
 module.exports.Place = require('./places')
 module.exports.Comment = require('./comment')
